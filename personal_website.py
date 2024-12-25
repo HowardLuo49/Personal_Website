592824c0cf724d262a9fed9907fecfc674151c16
Howard Luo <howardluo49@gmail.com>
2024-12-24 21:53:16 -0500

    initial page

--- personal_website.py
+++ personal_website.py
@@ -5,13 +5,8 @@ app = Flask(__name__)
 # Home page
 @app.route("/")
 def home():
-    return "<h1>Welcome to My Personal Website</h1><p>This is the home page.</p>"
-
-# About Me page
-@app.route("/about")
-def about():
-    return "<h1>About Me</h1><p>Here is some information about me.</p>"
+    return render_template("index.html")
 
 # Run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081)  # Use port 8081 for the second app
+    app.run(host="0.0.0.0", port=8081, debug=True)
