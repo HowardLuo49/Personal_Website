a620c837cd496a93f9d375bc5669a97d6220a275
Howard Luo <howardluo49@gmail.com>
2024-12-26 00:20:33 -0500

    basic template for all nav pages

--- personal_website.py
+++ personal_website.py
@@ -7,6 +7,31 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+@app.route("/coursework")
+def coursework():
+    return render_template("coursework.html")
+
+@app.route("/teaching")
+def teaching():
+    return render_template("teaching.html")
+
+@app.route("/research")
+def research():
+    return render_template("research.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 # Run the app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081, debug=True)
